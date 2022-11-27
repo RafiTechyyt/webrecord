eca7d5f488ad5d3b82cb53a3323ffa99f4260f34
Muhammed Rafi <90837440+RafiTechyyt@users.noreply.github.com>
2022-11-27 21:43:15 +0530

    Add files via upload

---
+++ php/simple.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>First PHP Page</title>
+    </head>
+    <body>
+    <h1>Muhammed Rafi </h1>
+            <h4>Roll No : 39</h4>
+        <?php echo "<h1>Muhammed Rafi </h2>"
+        ?>
+    </body>
+</html>
